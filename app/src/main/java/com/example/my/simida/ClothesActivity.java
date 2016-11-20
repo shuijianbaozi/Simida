98ffd816b62b7f93c3e7dedad44b164c1e49bc89
15898046020@163.com <ihavebro0508..ft>
2016-11-20 16:40:26 +0800

    1120

---
+++ app/src/main/java/com/example/my/simida/ClothesActivity.java
@@ -0,0 +1,13 @@
+package com.example.my.simida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClothesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothes);
+    }
+}
