07be73efdfc4f740140253916c92742b8c3c2878
Amanda-miao <Amanda-miao@foxmail.com>
2016-11-23 10:02:00 +0800

    Merge branch 'master' of F:\Android\11.14\Simida with conflicts.

---
+++ app/src/main/java/com/example/my/simida/ui/BuyActivity.java
@@ -0,0 +1,15 @@
+package com.example.my.simida.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.my.simida.R;
+
+public class BuyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy);
+    }
+}
